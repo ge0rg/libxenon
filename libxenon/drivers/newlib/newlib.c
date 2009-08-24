54ea7ab61e3b0404564e9362fdf3e5de5f1d5955
Felix Domke <felix.domke@multimedia-labs.de>
2009-08-24 12:04:25 +0200

    fix tabs, add stdout_hook

--- libxenon/drivers/newlib/newlib.c
+++ libxenon/drivers/newlib/newlib.c
@@ -44,6 +44,8 @@ static inline int is_valid_and_open_fd(int fd)
 	return fd >= 0 && fd < MAXFD && fd_array[fd].open;
 }
 
+void (*stdout_hook)(const char *text, int len) = 0;
+
 ssize_t write(int fildes, const void *buf, size_t nbyte)
 {
 //	printf("write(%d, %p, %ld)=", fildes, buf, nbyte);
@@ -56,6 +58,10 @@ ssize_t write(int fildes, const void *buf, size_t nbyte)
 	if (isatty(fildes))
 	{
 		const char *p = buf;
+
+		if (stdout_hook)
+			stdout_hook(p, nbyte);
+
 		int i = 0;
 		while (nbyte--)
 		{
@@ -121,29 +127,29 @@ off_t lseek(int fildes, off_t offset, int whence)
 
 	if (isatty(fildes))
 	{
-  	errno = ESPIPE;
-  	return ((off_t)-1);
-  }
-  
-  size_t filesize = ffs_files[fd_array[fildes].ffs_entry].size;
-  
-  switch (whence)
-  {
-  case 0:
-    fd_array[fildes].offset = offset;
-    break;
-  case 1:
-    fd_array[fildes].offset += offset;
-    break;
-  case 2:
-    fd_array[fildes].offset = filesize + offset;
-    break;
-  }
-  if (fd_array[fildes].offset < 0)
-    fd_array[fildes].offset = 0;
-  if (fd_array[fildes].offset >= filesize)
-    fd_array[fildes].offset = filesize;
-  return fd_array[fildes].offset;
+		errno = ESPIPE;
+		return ((off_t)-1);
+	}
+	
+	size_t filesize = ffs_files[fd_array[fildes].ffs_entry].size;
+	
+	switch (whence)
+	{
+	case 0:
+		fd_array[fildes].offset = offset;
+		break;
+	case 1:
+		fd_array[fildes].offset += offset;
+		break;
+	case 2:
+		fd_array[fildes].offset = filesize + offset;
+		break;
+	}
+	if (fd_array[fildes].offset < 0)
+		fd_array[fildes].offset = 0;
+	if (fd_array[fildes].offset >= filesize)
+		fd_array[fildes].offset = filesize;
+	return fd_array[fildes].offset;
 }
 
 ssize_t read(int fildes, void *buf, size_t nbyte)
@@ -159,21 +165,21 @@ ssize_t read(int fildes, void *buf, size_t nbyte)
 	if (isatty(fildes))
 	{
 //		printf("[EIO]\n");
-	  errno = EIO;
-	  return -1;
+		errno = EIO;
+		return -1;
 	}
 
-  size_t filesize = ffs_files[fd_array[fildes].ffs_entry].size;
-  off_t off = fd_array[fildes].offset;
-  
-  if (off + nbyte > filesize)
-    nbyte = filesize - off;
-  
-  memcpy(buf, ffs_files[fd_array[fildes].ffs_entry].content + off, nbyte);
-  fd_array[fildes].offset += nbyte;
-//  printf("%ld [%02x %02x %02x %02x]\n", nbyte, 
-//  	((unsigned char*)buf)[0], ((unsigned char*)buf)[1], ((unsigned char*)buf)[2], ((unsigned char*)buf)[3]);
-  return nbyte;
+	size_t filesize = ffs_files[fd_array[fildes].ffs_entry].size;
+	off_t off = fd_array[fildes].offset;
+	
+	if (off + nbyte > filesize)
+		nbyte = filesize - off;
+	
+	memcpy(buf, ffs_files[fd_array[fildes].ffs_entry].content + off, nbyte);
+	fd_array[fildes].offset += nbyte;
+//	printf("%ld [%02x %02x %02x %02x]\n", nbyte, 
+//		((unsigned char*)buf)[0], ((unsigned char*)buf)[1], ((unsigned char*)buf)[2], ((unsigned char*)buf)[3]);
+	return nbyte;
 }
 
 void _exit(int status)
@@ -188,45 +194,45 @@ int open(const char *path, int oflag, ...)
 	int fd = 0;
 	while (fd < MAXFD)
 	{
-	  if (!fd_array[fd].open)
-	    break;
+		if (!fd_array[fd].open)
+			break;
 		fd++;
 	}
-  if (fd == MAXFD)
-  {
-  	printf("[EMFILE]\n");
-    errno = EMFILE;
-  	return -1;
-  }
+	if (fd == MAXFD)
+	{
+		printf("[EMFILE]\n");
+		errno = EMFILE;
+		return -1;
+	}
 
-  fd_array[fd].write = 0;
+	fd_array[fd].write = 0;
 
 	if (oflag & O_CREAT)
 	{
 		printf("\n*** CREATE %d, %s\n", fd, path);
-	  fd_array[fd].ffs_entry = 0;
-	  fd_array[fd].offset = 0;
-	  fd_array[fd].open = 1;
-	  fd_array[fd].write = 1;
+		fd_array[fd].ffs_entry = 0;
+		fd_array[fd].offset = 0;
+		fd_array[fd].open = 1;
+		fd_array[fd].write = 1;
 		return fd;
 	}
-  
-  int i;
-  for (i = 0; i < ffs_files[i].filename; ++i)
-    if (!strcmp(path, ffs_files[i].filename))
-      break;
-  if (!ffs_files[i].filename)
-  {
-  	printf("[ENOENT]\n");
-    errno = ENOENT;
-    return -1;
-  }
+	
+	unsigned int i;
+	for (i = 0; ffs_files[i].filename; ++i)
+		if (!strcmp(path, ffs_files[i].filename))
+			break;
+	if (!ffs_files[i].filename)
+	{
+		printf("[ENOENT]\n");
+		errno = ENOENT;
+		return -1;
+	}
 
-  fd_array[fd].ffs_entry = i;
-  fd_array[fd].offset = 0;
-  fd_array[fd].open = 1;
-  printf("%d\n", fd);
-  return fd;
+	fd_array[fd].ffs_entry = i;
+	fd_array[fd].offset = 0;
+	fd_array[fd].open = 1;
+	printf("%d\n", fd);
+	return fd;
 }
 
 pid_t getpid(void)
