11190513a471c1a833ed797e92b67c770a7dc75f
Felix Domke <felix.domke@multimedia-labs.de>
2009-08-28 19:24:12 +0200

    add sound driver

---
+++ libxenon/drivers/xenon_sound/sound.c
@@ -0,0 +1,88 @@
+#include <pci/io.h>
+#include <string.h>
+#include <malloc.h>
+#include <xenon_smc/xenon_smc.h>
+#include <stdio.h>
+#include <ppc/cache.h>
+
+static inline uint32_t bswap32(uint32_t t)
+{
+	return ((t & 0xFF) << 24) | ((t & 0xFF00) << 8) | ((t & 0xFF0000) >> 8) | ((t & 0xFF000000) >> 24);
+}
+
+static int snd_base = 0xea001600, wptr, buffer_len;
+static void *buffer;
+
+void xenon_sound_init(void)
+{
+
+	static unsigned char smc_snd[32] = {0x8d, 1, 1};
+	xenon_smc_send_message(smc_snd);
+
+	uint32_t *descr = malloc(0x20 * 8);
+	int descr_base = ((int)descr) & 0x1FFFFFFF;
+	
+	buffer_len = 64*1024;
+	buffer = malloc(buffer_len);
+	
+	int buffer_base = ((int)buffer) & 0x1fffffff;
+	
+	int i;
+	for (i = 0; i < 0x20; ++i)
+	{
+		descr[i * 2] = bswap32(buffer_base + 0x800 * i);
+		descr[i * 2 + 1] = bswap32(0x80000000 | 0x800);
+	}
+	memdcbst(descr, 0x20 * 2 * 4);
+
+	write32(snd_base + 8, 0);
+	write32(snd_base + 8, 0x2000000);
+	write32(snd_base + 0, descr_base);
+	write32(snd_base + 8, 0x1d08001c);
+	write32(snd_base + 0xC, 0x1c);
+	write32(snd_base + 8, read32(snd_base) | 0x1000000);
+	
+	wptr = 0;
+}
+
+void xenon_sound_submit(void *data, int len)
+{
+	int i = 0;
+	while (len)
+	{
+		int av = buffer_len - wptr;
+		if (av > len)
+			av = len;
+		
+		memcpy(buffer + wptr, data + i, av);
+		memdcbst(buffer + wptr, av);
+		
+		i += av;
+		wptr += av;
+		len -= av;
+		if (wptr == buffer_len)
+			wptr = 0;
+	}
+	int cur_descr = wptr / 0x800;
+
+	write32(snd_base + 4, cur_descr << 8);
+	write32(snd_base + 8, read32(snd_base) | 0x1000000);
+}
+
+int xenon_sound_get_free(void)
+{
+	uint32_t reg = read32(snd_base + 4);
+	
+	int rptr_descr = reg & 0x1f;
+	int last_valid_descr = (reg & 0x1f00) >> 8;
+	int cur_len = (reg >> 16) & 0xFFFF;
+	
+	if (rptr_descr == last_valid_descr && !cur_len)
+		return buffer_len;
+
+	int rptr = rptr_descr * 0x800;
+	int av = rptr - wptr;
+	if (av < 0)
+		av += buffer_len;
+	return av;
+}
---
+++ libxenon/drivers/xenon_sound/sound.h
@@ -0,0 +1,18 @@
+#ifndef __xenon_sound_h
+#define __xenon_sound_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	/* you init, you get the nr. of LE, 16bit x 2 channels, signed audio bytes to write, and submit that as max. */
+
+void xenon_sound_init(void);
+void xenon_sound_submit(void *data, int len);
+int xenon_sound_get_free(void);
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif
