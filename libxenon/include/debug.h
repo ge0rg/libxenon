88d516f905d4a5f4d5eef138861d2176752820a3
GliGli <gligli@sfxteam.org>
2010-12-15 20:40:13 +0100

    debug: added breakpoint macro, just type BP at the beginning of a line to add a breakpoint

---
+++ libxenon/include/debug.h
@@ -0,0 +1,9 @@
+#ifndef __include_debug_h
+#define __include_debug_h
+
+#include <stdio.h>
+#include <xenon_uart/xenon_uart.h>
+
+#define BP {printf("[Breakpoint] in function %s, line %d, file %s\n",__FUNCTION__,__LINE__,__FILE__);getch();}
+
+#endif
