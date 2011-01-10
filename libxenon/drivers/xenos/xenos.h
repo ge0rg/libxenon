0d812748398a35611f84770aec7290f107e6bd39
GliGli <gligli@sfxteam.org>
2011-01-10 23:05:45 +0100

    xenos: adding 720p YUV resolution xenos: resolutions constants tidying

--- libxenon/drivers/xenos/xenos.h
+++ libxenon/drivers/xenos/xenos.h
@@ -9,14 +9,15 @@ extern "C" {
 #define VIDEO_MODE_VGA_640x480   0
 #define VIDEO_MODE_VGA_1024x768  1
 #define VIDEO_MODE_PAL60         2
-#define VIDEO_MODE_480p          3
+#define VIDEO_MODE_YUV_480P      3
 #define VIDEO_MODE_PAL50         4
 #define VIDEO_MODE_VGA_1280x768  5
 #define VIDEO_MODE_VGA_1360x768  6
 #define VIDEO_MODE_VGA_1280x720  7
 #define VIDEO_MODE_VGA_1440x900  8
 #define VIDEO_MODE_VGA_1280x1024 9
-#define VIDEO_MODE_HDMI_720p     10
+#define VIDEO_MODE_HDMI_720P     10
+#define VIDEO_MODE_YUV_720P      11
 
 void xenos_init(int videoMode);
 
