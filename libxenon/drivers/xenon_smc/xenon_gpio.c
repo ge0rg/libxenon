1c405e5b5040b8b7c5af8293fe152905e8b56fe2
GliGli <gligli@sfxteam.org>
2011-04-20 20:48:31 +0200

    smc gpios: give access to more regs

--- libxenon/drivers/xenon_smc/xenon_gpio.c
+++ libxenon/drivers/xenon_smc/xenon_gpio.c
@@ -3,6 +3,15 @@
 
 #define SMC_BASE 0xea001000
 
+void xenon_gpio_control(uint32_t reg, uint32_t clear, uint32_t set)
+{
+	int r[]={0x20,0x24,0x28,0x30,0x34,0x38,0x40,0x44,0x48};
+	int b = read32(SMC_BASE | r[reg]);
+	b &= ~clear;
+	b |= set;
+	write32(SMC_BASE | r[reg], b);
+}
+
 void xenon_gpio_set_oe(uint32_t clear, uint32_t set)
 {
 	write32(SMC_BASE + 0x20, (read32(SMC_BASE + 0x20) &~ clear) | set);
--- libxenon/drivers/xenon_smc/xenon_gpio.h
+++ libxenon/drivers/xenon_smc/xenon_gpio.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 
+void xenon_gpio_control(uint32_t reg, uint32_t clear, uint32_t set);
 void xenon_gpio_set_oe(uint32_t clear, uint32_t set);
 void xenon_gpio_set(uint32_t clear, uint32_t set);
 
