a3e54e999e50c06d43baf680f4348cf777eff5e9
GliGli <gligli@sfxteam.org>
2011-04-20 20:52:12 +0200

    usb: more oops

---
+++ libxenon/drivers/usb/usbmain.h
@@ -0,0 +1,16 @@
+#ifndef USBMAIN_H
+#define	USBMAIN_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+int usb_init(void);
+void usb_do_poll(void);
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* USBMAIN_H */
+
