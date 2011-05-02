63165b9b812ef0060dc6e4e039a7fb0560e0338f
GliGli <gligli@sfxteam.org>
2011-05-02 22:59:07 +0200

    elf loader: linux support

--- libxenon/drivers/elf/elf.c
+++ libxenon/drivers/elf/elf.c
@@ -10,36 +10,57 @@
 
 #include "elf_abi.h"
 
-#define ELF_CODE_RELOC_START ((void*)0x87FF0000)
+#define ELF_DEVTREE_START ((void*)0x87FE0000)
+#define ELF_DEVTREE_MAX_SIZE 0x10000
+
+#define ELF_CODE_RELOC_START ((void*)0x87FF0000) /* TODO: must keep this synced with lis %r4,0x87ff and lis %r4,0x07ff in elf_run.S */
 #define ELF_DATA_RELOC_START ((void*)0x88000000)
 #define ELF_GET_RELOCATED(x) (ELF_CODE_RELOC_START+((unsigned long)(x)-(unsigned long)elfldr_start))
 
 extern char elfldr_start[], elfldr_end[];
-extern void elf_run(unsigned long entry);
+extern void elf_run(unsigned long entry,unsigned long devtree);
 extern void elf_hold_thread();
 
 extern volatile unsigned long elf_secondary_hold_addr;
 
-static inline void elf_putch(unsigned char c)
+static inline __attribute__((always_inline)) void elf_putch(unsigned char c)
 {
 	while (!((*(volatile uint32_t*)(0xea001000 + 0x18))&0x02000000));
 	*(volatile uint32_t*)(0xea001000 + 0x14) = (c << 24) & 0xFF000000;
 }
 
-static inline void elf_memset(unsigned char *dst, int v, int l)
+static inline __attribute__((always_inline)) void elf_puts(unsigned char *s)
+{
+	while(*s) elf_putch(*s++);
+}
+
+static inline __attribute__((always_inline)) void elf_int2hex(unsigned long long n,unsigned char w,unsigned char *str)
+{
+  unsigned char i,d;
+  
+  str+=w;
+  *str=0;
+
+  for(i=0;i<w;++i){
+  	d= ( n >> (i << 2) ) & 0x0f;
+    *--str= (d<=9)?d+48:d+55;
+  };
+}
+
+
+static inline __attribute__((always_inline)) void elf_memset(unsigned char *dst, int v, int l)
 {
 	while (l--) *dst++ = v;
 }
 
-static inline void elf_memcpy(unsigned char *dst, const unsigned char *src, int l)
+static inline __attribute__((always_inline)) void elf_memcpy(unsigned char *dst, const unsigned char *src, int l)
 {
 	while (l--)
 		*dst++ = *src++;
 }
 
-#if 1
 #define LINESIZE 128
-static inline void elf_sync(volatile void *addr)
+static inline __attribute__((always_inline)) void elf_sync(volatile void *addr)
 {
 	asm volatile ("dcbst 0, %0" : : "b" (addr));
 	asm volatile ("sync");
@@ -47,7 +68,7 @@ static inline void elf_sync(volatile void *addr)
 	asm volatile ("isync");
 }
       
-static inline void elf_sync_before_exec(unsigned char *dst, int l)
+static inline __attribute__((always_inline)) void elf_sync_before_exec(unsigned char *dst, int l)
 {
 	dst=(unsigned char *)((unsigned long)dst&(~(LINESIZE-1)));
 	
@@ -61,29 +82,11 @@ static inline void elf_sync_before_exec(unsigned char *dst, int l)
 		l -= LINESIZE;
 	}
 }
-#else
-#define LINESIZE 16
-static inline void elf_sync(volatile void *addr)
-{
-	asm volatile ("dcbst 0, %0" : : "b" (addr));
-	asm volatile ("sync");
-	asm volatile ("icbi 0, %0" : : "b" (addr));
-	asm volatile ("isync");
-}
-      
-static inline void elf_sync_before_exec(unsigned char *dst, int l)
-{
-	while (l > LINESIZE)
-	{
-		elf_sync(dst);
-		dst += LINESIZE;
-		l -= LINESIZE;
-	}
-}
-#endif
 
 static void __attribute__ ((section (".elfldr"))) elf_prepare_run (void *addr)
 {
+	unsigned char s[50];
+	
 	Elf32_Ehdr *ehdr;
 	Elf32_Shdr *shdr;
 	unsigned char *strtab = 0;
@@ -104,6 +107,16 @@ static void __attribute__ ((section (".elfldr"))) elf_prepare_run (void *addr)
 		if (!(shdr->sh_flags & SHF_ALLOC) || shdr->sh_size == 0)
 			continue;
 
+		elf_int2hex(shdr->sh_addr,8,s);
+		elf_puts(s);
+		elf_putch(' ');
+		elf_int2hex(shdr->sh_size,8,s);
+		elf_puts(s);
+		elf_putch(' ');
+		elf_puts(&strtab[shdr->sh_name]);
+		elf_putch('\r');
+		elf_putch('\n');
+
 		void *target = (void*)(((unsigned long)0x80000000) | shdr->sh_addr);
 
 		if (shdr->sh_type == SHT_NOBITS) {
@@ -115,12 +128,21 @@ static void __attribute__ ((section (".elfldr"))) elf_prepare_run (void *addr)
 		}
 		elf_sync_before_exec (target, shdr->sh_size);
 	}
+	elf_putch('\r');
+	elf_putch('\n');
+	elf_putch('E');
+	elf_putch('P');
+	elf_putch(' ');
+	elf_int2hex(ehdr->e_entry,8,s);
+	elf_puts(s);
+	elf_putch('\r');
+	elf_putch('\n');
 
 	*(volatile unsigned long *)ELF_GET_RELOCATED(&elf_secondary_hold_addr) = ehdr->e_entry + 0x60;
 	
 	// call elf_run()
-	void(*call)(unsigned long) = ELF_GET_RELOCATED(elf_run);
-	call(ehdr->e_entry);
+	void(*call)(unsigned long,unsigned long) = ELF_GET_RELOCATED(elf_run);
+	call(ehdr->e_entry,((unsigned long)ELF_DEVTREE_START)&0x7fffffff);
 }
 
 void elf_runFromMemory (void *addr, int size)
@@ -173,4 +195,17 @@ int elf_runFromDisk (char *filename)
 	elf_runFromMemory(buf, r);
 
 	return 0;
+}
+
+void elf_runWithDeviceTree (void *elf_addr, int elf_size, void *dt_addr, int dt_size)
+{
+	if (dt_size>ELF_DEVTREE_MAX_SIZE){
+		printf("[ELF loader] Device tree too big (> %d bytes) !\n",ELF_DEVTREE_MAX_SIZE);
+		return;
+	}		
+	memset(ELF_DEVTREE_START,0,ELF_DEVTREE_MAX_SIZE);
+	memcpy(ELF_DEVTREE_START,dt_addr,dt_size);
+	memdcbst(ELF_DEVTREE_START,ELF_DEVTREE_MAX_SIZE);
+	
+	elf_runFromMemory(elf_addr,elf_size);
 }
--- libxenon/drivers/elf/elf.h
+++ libxenon/drivers/elf/elf.h
@@ -8,7 +8,7 @@ extern "C" {
 
 void elf_runFromMemory (void *addr, int size);
 int elf_runFromDisk (char *filename);
-
+void elf_runWithDeviceTree (void *elf_addr, int elf_size, void *dt_addr, int dt_size);
 
 #ifdef	__cplusplus
 }
