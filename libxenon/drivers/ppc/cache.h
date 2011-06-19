319b756d1299dfc322a2ced85e5ff8f025cdf2ba
GliGli <gligli@sfxteam.org>
2011-06-19 15:37:39 +0200

    cache: added memdcbt

--- libxenon/drivers/ppc/cache.h
+++ libxenon/drivers/ppc/cache.h
@@ -1,6 +1,7 @@
 #ifndef __drivers_ppc_cache_cache_h
 #define __drivers_ppc_cache_cache_h
 
+extern void memdcbt(void *addr, int len);
 extern void memdcbst(void *addr, int len);
 extern void memdcbf(void *addr, int len);
 extern void memicbi(void *addr, int len);
