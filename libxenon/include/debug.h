43c1811616c2091aa8bec079146aab6e5d7f525e
GliGli <gligli@sfxteam.org>
2011-07-17 21:19:09 +0200

    debug: adding TR macro (trace)

--- libxenon/include/debug.h
+++ libxenon/include/debug.h
@@ -5,6 +5,7 @@
 #include <xenon_uart/xenon_uart.h>
 
 #define BP {printf("[Breakpoint] in function %s, line %d, file %s\n",__FUNCTION__,__LINE__,__FILE__);getch();}
+#define TR {printf("[Trace] in function %s, line %d, file %s\n",__FUNCTION__,__LINE__,__FILE__);}
 
 void buffer_dump(void * buf, int size);
 
