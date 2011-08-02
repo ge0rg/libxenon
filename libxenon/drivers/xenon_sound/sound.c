b839dbeb6b5ab5727218cf4fe2c7f2969cafcc85
Ced2911 <cc2911@facebook.com>
2011-08-03 01:46:11 +0800

    Added i2c for hdmi sound

--- libxenon/drivers/xenon_sound/sound.c
+++ libxenon/drivers/xenon_sound/sound.c
@@ -15,6 +15,20 @@ void xenon_sound_init(void)
 	xenon_gpio_control(5,0x1000,0x1000);
 	xenon_gpio_control(0,0x1000,0x1000);
 	xenon_gpio_control(4,0x1000,0x1000);
+
+	xenon_smc_i2c_write(0x0214, 0x11);
+	xenon_smc_i2c_write(0x0202, 0x03);
+	xenon_smc_i2c_write(0x0203, 0x00);
+	xenon_smc_i2c_write(0x0204, 0x18);
+	xenon_smc_i2c_write(0x0205, 0x00);
+	xenon_smc_i2c_write(0x021d, 0x40);
+	xenon_smc_i2c_write(0x0221, 0x02);
+	xenon_smc_i2c_write(0x0222, 0x2b);
+
+	xenon_smc_i2c_write(0x022f,0x01);
+	xenon_smc_i2c_write(0x023e,0x3f);
+ 
+	xenon_smc_i2c_write(0x02df,0x10);// Av mute ?
 	
 	static unsigned char smc_snd[32] = {0x8d, 1, 1};
 	xenon_smc_send_message(smc_snd);
