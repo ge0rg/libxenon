a09c268fc27fa4486772b2a52c69fca40cd774b2
Austin Morton <amorton@juvsoft.com>
2012-05-31 09:28:25 -0400

    POST now works from C and ASM, with the same macro

--- libxenon/drivers/xenon_post/xenon_post.h
+++ libxenon/drivers/xenon_post/xenon_post.h
@@ -1,5 +1,13 @@
 #ifndef __xenon_post_h
 #define __xenon_post_h
+#ifdef __ASSEMBLER__
+#define POST(c) \
+	li		%r3, c; \
+	sldi	%r3, %r3, 56; \
+	lis		%r4, 0x2006; \
+	ori		%r4, %r4, 0x1010; \
+	std		%r3, 0(%r4)
+#else
 
 #ifdef __cplusplus
 extern "C" {
@@ -11,6 +19,8 @@ void xenon_post(char post_code);
 
 #ifdef __cplusplus
 };
+#endif
+
 #endif
 
 #endif
