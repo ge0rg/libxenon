1ca5031463e3aaaef79527687dc16e59494e2cf4
GliGli <gligli@sfxteam.org>
2012-09-02 22:57:02 +0200

    New and improved debug tools

--- libxenon/drivers/utils/debug.c
+++ libxenon/drivers/utils/debug.c
@@ -17,3 +17,36 @@ void buffer_dump(void * buf, int size){
 	}
 }
 
+extern unsigned char elfldr_start[]; // also end of .text ...
+
+static int seems_valid(unsigned int p){
+	return p>=0x80000000 && p<(unsigned int)elfldr_start;
+}
+
+#define DO_RA(x) if(seems_valid(addr) && x<max_depth){ addr=(unsigned int)__builtin_return_address(x); printf("%p; ",addr); }
+
+void stack_trace(int max_depth)
+{
+    unsigned int addr=0x80000000;
+    
+    printf("[Stack trace] ");
+    
+    DO_RA(0);
+    DO_RA(1);
+    DO_RA(2);
+    DO_RA(3);
+    DO_RA(4);
+    DO_RA(5);
+    DO_RA(6);
+    DO_RA(7);
+    DO_RA(8);
+    DO_RA(9);
+    DO_RA(10);
+    DO_RA(11);
+    DO_RA(12);
+    DO_RA(13);
+    DO_RA(14);
+    DO_RA(15);
+    
+    printf("\n");
+}
